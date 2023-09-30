773f68b5cd67f9082ddd663a19257bcef7dd0f4d
Rami <ramihomsi1997@hotmail.com>
2023-09-30 12:35:49 +0100

    first commit

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import style from "./page.module.css";
+
+export default function Contact() {
+  return <div className={style.Contact}>Contact</div>;
+}
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <div>©2023 RAmi. All rights reserved.</div>
+    </div>
+  );
+}
