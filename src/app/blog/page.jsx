e14130b4c27ce702d99cb1644e2ea5a80db97143
RAmiHomsi <ramihomsi1997@hotmail.com>
2024-02-27 19:07:07 +0100

    deploy

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,45 +1,52 @@
 import React from "react";
-import style from "./page.module.css";
-import Image from "next/image";
+import styles from "./page.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
-export default async function Blog() {
-  async function getData() {
-    const res = await fetch("http://localhost:3000/api/posts", {
-      cache: "no-store",
-    });
+//Prerender Error Fix
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
-    }
+async function getData() {
+  const res = await fetch("http://localhost:3000/api/posts", {
+    cache: "no-store",
+  });
 
-    return res.json();
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
   }
 
+  return res.json();
+}
+
+const Blog = async () => {
   const data = await getData();
+  if (!data) return notFound();
   return (
-    <div>
+    <div className={styles.mainContainer}>
       {data.map((item) => (
         <Link
           href={`/blog/${item._id}`}
-          className={style.container}
+          className={styles.container}
           key={item.id}
         >
-          <div className={style.imageContainer}>
+          <div className={styles.imageContainer}>
             <Image
               src={item.img}
               alt=""
               width={400}
               height={250}
-              className={style.image}
+              className={styles.image}
             />
           </div>
-          <div className={style.content}>
-            <h1 className={style.title}>{item.title}</h1>
-            <p className={style.desc}>{item.desc}</p>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
           </div>
         </Link>
       ))}
     </div>
   );
-}
+};
+
+export default Blog;
